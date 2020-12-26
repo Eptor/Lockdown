c73a226965039ac209b2a0b68769d4773f6849df
Hector <hector_espinoza02@hotmail.com>
2020-12-25 18:09:49 -0800

    Instalación importada al main script

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from modules.data_handler import (
     recovery,
     edit_data,
 )
+from modules.install import install
 
 # Modulos vanila
 import os
@@ -50,165 +51,180 @@ def menu():
     print("\nCTRL + C) - Salir\n")
 
 
-# Login
-while True:
-    os.system("cls")
-    try:
-        user = input("Usuario: ")
-        key = getpass("Contraseña: ")
+def main():
+    # Login
+    while True:
+        os.system("cls")
+        try:
+            user = input("Usuario: ")
+            key = getpass("Contraseña: ")
 
-        if user == "help" and key == "":  # Recuperar contraseña
-            user = input("Introduce tu usuario: ")
-            mn = input("> Introduce tu clave mnemotécnica:\n> ")
-            os.system("cls")
-            print(
-                Fore.YELLOW
-                + f"Tu contraseña es: {decrypt(recovery(user), mn)}"
-            )
-            input("Presiona enter para continuar")
-
-        elif user == "help" and key == "backup":  # Usar nueva base de datos
-
-            print(Fore.YELLOW + "Espera mientras leemos el archivo")
-
-            with open(
-                "database/data_backup.lockdown", "rb"
-            ) as data_read:  # Leer bytes del archivo para desencriptado
-                data = decrypt_bkp(
-                    data_read.read(), input("Clave mnemotécnica\n> ")
+            if user == "help" and key == "":  # Recuperar contraseña
+                user = input("Introduce tu usuario: ")
+                mn = input("> Introduce tu clave mnemotécnica:\n> ")
+                os.system("cls")
+                print(
+                    Fore.YELLOW
+                    + f"Tu contraseña es: {decrypt(recovery(user), mn)}"
                 )
+                input("Presiona enter para continuar")
 
-            with open(
-                "database/data.sqlite", "wb"
-            ) as data_write:  # Sobre-escribir la base de datos actual con la nueva
-                data_write.write(data)
+            elif (
+                user == "help" and key == "backup"
+            ):  # Usar nueva base de datos
 
-            print(Fore.CYAN + "Base de datos restaurada")
-            sleep(2)
+                print(Fore.YELLOW + "Espera mientras leemos el archivo")
 
-        elif key == decrypt(get_user_data(user), key):  # Login exitoso
-            print(Fore.GREEN + "\nLogin exitoso")
+                with open(
+                    "database/data_backup.lockdown", "rb"
+                ) as data_read:  # Leer bytes del archivo para desencriptado
+                    data = decrypt_bkp(
+                        data_read.read(), input("Clave mnemotécnica\n> ")
+                    )
+
+                with open(
+                    "database/data.sqlite", "wb"
+                ) as data_write:  # Sobre-escribir la base de datos actual con la nueva
+                    data_write.write(data)
+
+                print(Fore.CYAN + "Base de datos restaurada")
+                sleep(2)
+
+            elif key == decrypt(get_user_data(user), key):  # Login exitoso
+                print(Fore.GREEN + "\nLogin exitoso")
+                sleep(1)
+                print(Fore.CYAN + "Bienvenido a Lockdown")
+                sleep(1)
+                break
+
+            else:
+                continue
+
+        except Exception:
+            print(Fore.RED + "\nDatos erroneos.")
             sleep(1)
-            break
 
-        else:
-            continue
+    # Main loop
+    try:
+        while True:
+            menu()
+            om = input("> ")
+            if om == "1":  # Ver datos
+                table = PrettyTable()  # Creacion de la tabla
+                os.system("cls")
+                try:
+                    data = get_password_data(
+                        input("Nombre: ")
+                    )  # Obtencion de datos
+                except IndexError:
+                    print(Fore.RED + "\nNo existen registros con ese nombre !")
+                    sleep(2)
+                else:
+                    table.field_names = [
+                        "Nombre",
+                        "Email",
+                        "Contraseña",
+                        "Link",
+                    ]
+                    table.add_row(
+                        [
+                            data[0],
+                            decrypt(data[1], key),
+                            decrypt(data[2], key),
+                            data[3],
+                        ]
+                    )
+                    print()
+                    try:
+                        print(table)
+                        input(
+                            "\nPresiona ENTER para regresar al menu\nPresiona CTRL + C para copiar la contraseña"
+                        )
+                    except KeyboardInterrupt:
+                        pyperclip.copy(decrypt(data[2], key))
+                    else:
+                        del table
 
-    except Exception:
-        print(Fore.RED + "\nDatos erroneos.")
+            elif om == "2":
+                os.system("cls")
+                try:
+                    insert_data(key)  # Añadir registro
+                except Exception as e:
+                    print(Fore.RED + "Error al añadir")
+                    print(e)
+                else:
+                    print(Fore.GREEN + "\nAñadido con exito.")
+                sleep(2)
+
+            elif om == "3":
+                os.system("cls")
+                if delete_data():  # Elimianr registro
+                    print(Fore.GREEN + "\nEliminado con exito.")
+                    sleep(2)
+
+                else:
+                    print(Fore.RED + "\nNo ha sido eliminado.")
+                    sleep(2)
+
+            elif om == "4":
+                os.system("cls")
+                cambiar = input("Editar: ")  # Cambiar datos de registro
+
+                if cambiar != "":
+                    if edit_data(cambiar, key):
+                        print(Fore.GREEN + "\nEditado con exito!")
+                        sleep(2)
+
+                    else:
+                        print(Fore.RED + "\nNo fue editado.")
+                        sleep(2)
+                else:
+                    print(Fore.YELLOW + "Volviendo al menu.")
+                    sleep(1)
+
+            elif om == "5":  # Generador de contraseñas
+                os.system("cls")
+                print(Fore.YELLOW + "Vamos a generarte una contraseña nueva !")
+                sleep(1)
+                password = generador()
+                print("\nTu contraseña es:", Fore.MAGENTA + password)
+                sleep(1)
+                pyperclip.copy(password)
+                print(Fore.CYAN + "\nLa copiamos a tu portapapeles !")
+                sleep(2)
+
+            elif om == "BKP":  # Respaldo
+                os.system("cls")
+                print(Fore.YELLOW + "Espera a que realicemos el backup")
+
+                with open(
+                    "database/data.sqlite", "rb"
+                ) as data_read:  # Leer los bytes del archivo para encriptarlos
+                    data = encrypt(
+                        data_read.read(), input("Clave mnemotécnica\n> ")
+                    )
+
+                with open(
+                    "database/data_backup.lockdown", "wb"
+                ) as data_write:  # Escribe los bytes encriptados en un nuevo archivo
+                    data_write.write(data.encode())
+
+                print("Completado.")
+                sleep(2)
+
+            else:
+                pass
+
+    except KeyboardInterrupt:
+        os.system("cls")
+        print(Fore.CYAN + "Gracias por usar Lockdown!")
+        sleep(1)
+        print("\nSaliendo.")
         sleep(1)
 
 
-# Main loop
-try:
-    while True:
-        menu()
-        om = input("> ")
-        if om == "1":  # Ver datos
-            table = PrettyTable()  # Creacion de la tabla
-            os.system("cls")
-            try:
-                data = get_password_data(
-                    input("Nombre: ")
-                )  # Obtencion de datos
-            except IndexError:
-                print(Fore.RED + "\nNo existen registros con ese nombre !")
-                sleep(2)
-            else:
-                table.field_names = ["Nombre", "Email", "Contraseña", "Link"]
-                table.add_row(
-                    [
-                        data[0],
-                        decrypt(data[1], key),
-                        decrypt(data[2], key),
-                        data[3],
-                    ]
-                )
-                print()
-                try:
-                    print(table)
-                    input(
-                        "\nPresiona ENTER para regresar al menu\nPresiona CTRL + C para copiar la contraseña"
-                    )
-                except KeyboardInterrupt:
-                    pyperclip.copy(decrypt(data[2], key))
-                else:
-                    del table
-
-        elif om == "2":
-            os.system("cls")
-            try:
-                insert_data(key)  # Añadir registro
-            except Exception as e:
-                print(Fore.RED + "Error al añadir")
-                print(e)
-            else:
-                print(Fore.GREEN + "\nAñadido con exito.")
-            sleep(2)
-
-        elif om == "3":
-            os.system("cls")
-            if delete_data():  # Elimianr registro
-                print(Fore.GREEN + "\nEliminado con exito.")
-                sleep(2)
-
-            else:
-                print(Fore.RED + "\nNo ha sido eliminado.")
-                sleep(2)
-
-        elif om == "4":
-            os.system("cls")
-            cambiar = input("Editar: ")  # Cambiar datos de registro
-
-            if cambiar != "":
-                if edit_data(cambiar, key):
-                    print(Fore.GREEN + "\nEditado con exito!")
-                    sleep(2)
-
-                else:
-                    print(Fore.RED + "\nNo fue editado.")
-                    sleep(2)
-            else:
-                print(Fore.YELLOW + "Volviendo al menu.")
-                sleep(1)
-
-        elif om == "5":  # Generador de contraseñas
-            os.system("cls")
-            print(Fore.YELLOW + "Vamos a generarte una contraseña nueva !")
-            sleep(1)
-            password = generador()
-            print("\nTu contraseña es:", Fore.MAGENTA + password)
-            sleep(1)
-            pyperclip.copy(password)
-            print(Fore.CYAN + "\nLa copiamos a tu portapapeles !")
-            sleep(2)
-
-        elif om == "BKP":  # Respaldo
-            os.system("cls")
-            print(Fore.YELLOW + "Espera a que realicemos el backup")
-
-            with open(
-                "database/data.sqlite", "rb"
-            ) as data_read:  # Leer los bytes del archivo para encriptarlos
-                data = encrypt(
-                    data_read.read(), input("Clave mnemotécnica\n> ")
-                )
-
-            with open(
-                "database/data_backup.lockdown", "wb"
-            ) as data_write:  # Escribe los bytes encriptados en un nuevo archivo
-                data_write.write(data.encode())
-
-            print("Completado.")
-            sleep(2)
-
-        else:
-            pass
-
-except KeyboardInterrupt:
-    os.system("cls")
-    print(Fore.CYAN + "Gracias por usar Lockdown!")
-    sleep(1)
-    print("\nSaliendo.")
-    sleep(1)
-    quit()
+if __name__ == "__main__":
+    if os.path.isfile("database/data.sqlite"):
+        main()
+    else:
+        install()
--- modules/install.py
+++ modules/install.py
@@ -5,106 +5,107 @@ from getpass import getpass
 from os import system, path
 
 # Modulos externos (pip install)
-from crypto import mnemotecnica, encrypt
+from modules.crypto import mnemotecnica, encrypt
 from colorama import init, Fore
 import pyperclip
 
 
-init(autoreset=True)  # Colorama init
+def install():
+    init(autoreset=True)  # Colorama init
 
-if path.isfile(
-    "database/data.sqlite"
-):  # Verificar si ya existe una instalación
-    print("Ya existe una instalación.")
-    quit()
+    if path.isfile(
+        "database/data.sqlite"
+    ):  # Verificar si ya existe una instalación
+        print("Ya existe una instalación.")
+        quit()
 
-else:
-    system("cls")
-    print(Fore.CYAN + "Bienvenido a la instalación de Lockdown!")
-    print("\nVamos a comenzar\n")
-    sleep(1)
+    else:
+        system("cls")
+        print(Fore.CYAN + "Bienvenido a la instalación de Lockdown!")
+        print("\nVamos a comenzar\n")
+        sleep(1)
 
-    # Conexion de la base de datos
-    conn = sqlite3.connect("database/data.sqlite")
-    cursor = conn.cursor()
+        # Conexion de la base de datos
+        conn = sqlite3.connect("database/data.sqlite")
+        cursor = conn.cursor()
 
-    # Creacion de la tabla usuarios
-    print("[+] Creando la tabla de usuarios.\n")
-    cursor.execute(
+        # Creacion de la tabla usuarios
+        print("[+] Creando la tabla de usuarios.\n")
+        cursor.execute(
+            """
+            CREATE TABLE "users" (
+            "user"	TEXT NOT NULL UNIQUE,
+            "password"	TEXT NOT NULL,
+            "backup" TEXT
+        )
         """
-        CREATE TABLE "users" (
-	    "user"	TEXT NOT NULL UNIQUE,
-	    "password"	TEXT NOT NULL,
-        "backup" TEXT
-    )
-    """
-    )
-    sleep(1)
-    print(Fore.GREEN + "Listo!\n")
-    sleep(1)
+        )
+        sleep(1)
+        print(Fore.GREEN + "Listo!\n")
+        sleep(1)
 
-    # Creacion de la tabla data
-    print("[+] Creando la tabla de almacenamiento\n")
-    cursor.execute(
+        # Creacion de la tabla data
+        print("[+] Creando la tabla de almacenamiento\n")
+        cursor.execute(
+            """
+        CREATE TABLE "data" (
+            "Nombre" TEXT NOT NULL,
+            "Email" TEXT,
+            "Contraseña" TEXT NOT NULL,
+            "Link" TEXT
+        )
         """
-    CREATE TABLE "data" (
-        "Nombre" TEXT NOT NULL,
-        "Email" TEXT,
-        "Contraseña" TEXT NOT NULL,
-        "Link" TEXT
-    )
-    """
-    )
-    sleep(1)
-    print(Fore.GREEN + "Listo!\n")
-    sleep(2)
+        )
+        sleep(1)
+        print(Fore.GREEN + "Listo!\n")
+        sleep(2)
 
-    # Creacion del usuario
-    print("Introduzca su nombre de usuario")
-    usuario = input("> ")
-    print(Fore.GREEN + f"\nBienvenido {Fore.RESET+usuario}!\n")
-    sleep(1)
+        # Creacion del usuario
+        print("Introduzca su nombre de usuario")
+        usuario = input("> ")
+        print(Fore.GREEN + f"\nBienvenido {Fore.RESET+usuario}!\n")
+        sleep(1)
 
-    while True:  # Loop para verificar que la contraseña esté bien escrita
-        print("Ahora introduce tu contraseña")
-        passwd = getpass("> ")
-        print("Confirmala")
-        passw2 = getpass("> ")
+        while True:  # Loop para verificar que la contraseña esté bien escrita
+            print("Ahora introduce tu contraseña")
+            passwd = getpass("> ")
+            print("Confirmala")
+            passw2 = getpass("> ")
 
-        if passwd != passw2:
-            print("Las contraseñas no coinciden")
-            system("cls")
-            continue
+            if passwd != passw2:
+                print("Las contraseñas no coinciden")
+                system("cls")
+                continue
 
-        else:
-            break
+            else:
+                break
 
-    # Creacion de la clave mnemotecnica
-    print("Ahora vamos a crear tu clave mnemotécnica.")
-    sleep(3)
-    print(
-        f"\nEsta servirá para recuperar tu cuenta en caso de que olvides tu contraseña, por lo que es de suma importancia que {Fore.RED+'NO LA PIERDAS'}."
-    )
-    sleep(4)
+        # Creacion de la clave mnemotecnica
+        print("Ahora vamos a crear tu clave mnemotécnica.")
+        sleep(3)
+        print(
+            f"\nEsta servirá para recuperar tu cuenta en caso de que olvides tu contraseña, por lo que es de suma importancia que {Fore.RED+'NO LA PIERDAS'}."
+        )
+        sleep(4)
 
-    print("\nTu clave es:\n")
-    key = (
-        mnemotecnica()
-    )  # Genera la clave mnemotecnica (ver funcion en archivo crypto.py)
-    print(Fore.YELLOW + key)
-    pyperclip.copy(key)  # Se copia la clave al portapapeles
+        print("\nTu clave es:\n")
+        key = (
+            mnemotecnica()
+        )  # Genera la clave mnemotecnica (ver funcion en archivo crypto.py)
+        print(Fore.YELLOW + key)
+        pyperclip.copy(key)  # Se copia la clave al portapapeles
 
-    # Anucnio de importancia
-    print("\nPara tu comodidad, la hemos copiado a tu portapapeles.")
-    input("\nCuando estés listo para continuar, presiona {ENTER}")
-    print("\nAhora te añadiremos a la base de datos, aguarda un momento.")
+        # Anucnio de importancia
+        print("\nPara tu comodidad, la hemos copiado a tu portapapeles.")
+        input("\nCuando estés listo para continuar, presiona {ENTER}")
+        print("\nAhora te añadiremos a la base de datos, aguarda un momento.")
 
-    # Creación del usuario en la base de datos
-    key = input("Introduce tu clave mnemotécnica:\n> ")
-    cursor.execute(
-        f"INSERT INTO users VALUES ('{usuario}', '{encrypt(passwd, passwd)}', '{encrypt(passwd, key)}')"
-    )
-    sleep(2)
-    conn.commit()
+        # Creación del usuario en la base de datos
+        key = input("Introduce tu clave mnemotécnica:\n> ")
+        cursor.execute(
+            f"INSERT INTO users VALUES ('{usuario}', '{encrypt(passwd, passwd)}', '{encrypt(passwd, key)}')"
+        )
+        sleep(2)
+        conn.commit()
 
-    print(Fore.CYAN + "\nInstalación completada!")
+        print(Fore.CYAN + "\nInstalación completada!")
